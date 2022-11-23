ad8ef461a9ca47c4a20f76524d44547f04fedfc2
Victor Banerjee <recervictory@gmail.com>
2022-11-23 17:17:25 +0530

    React Router Created

--- react-blog/src/App.tsx
+++ react-blog/src/App.tsx
@@ -1,16 +1,30 @@
+//* Import Pages
+import HomePage from "./pages/HomePage";
+import AboutPage from "./pages/AboutPage";
+import ArticlePage from "./pages/ArticlePage";
+import ArticleListPage from "./pages/ArticleListPage";
 
-import './App.css'
+//* Import CSS
+import "./App.css";
+//* React Router
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
-  
   return (
-    <div className="App">
-    <h1>My Blog</h1>
-    <div id="page-body">
-      WELCOME TO MY BlOG!
-    </div>
-    </div>
-  )
+    <BrowserRouter>
+      <div className="App">
+        <h1>My Blog</h1>
+        <div id="page-body">
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/about" element={<AboutPage/>} /> 
+            <Route path="/articles" element={<ArticleListPage />} />
+            <Route path="/articles/:articleId" element={<ArticlePage/>} />
+          </Routes>
+        </div>
+      </div>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
