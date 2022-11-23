07c3629b4c6b1a70c52e9cb5043c2033e9f62048
Victor Banerjee <recervictory@gmail.com>
2022-11-23 18:11:09 +0530

    Modular Articles

--- react-blog/src/pages/ArticleListPage.tsx
+++ react-blog/src/pages/ArticleListPage.tsx
@@ -1,16 +1,13 @@
 import React from 'react'
 import articles from '../assets/articles';
 import {Link} from 'react-router-dom';
+import ArticlesList from '../components/ArticlesList';
 
 const ArticleListPage = () => {
   return (
     <div>
-      {articles.map(article => (
-      <Link key={article.name} className="article-list-item" to={`/articles/${article.name}`}>
-        <h3>{article.title}</h3>
-        <p>{article.content[0].substring(0,150)}</p>
-      </Link>
-    ))}
+      <h1>Articles</h1>
+      <ArticlesList articles={articles} />
     </div>
   )
 }
