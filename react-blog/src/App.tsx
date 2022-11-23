49b1b5c53cb8a1d1b20b737841ec62896e8d9949
Victor Banerjee <recervictory@gmail.com>
2022-11-23 16:50:22 +0530

    01 - React App Reacted

---
+++ react-blog/src/App.tsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+function App() {
+  
+  return (
+    <div className="App">
+    <h1>My Blog</h1>
+    <div id="page-body">
+      WELCOME TO MY BlOG!
+    </div>
+    </div>
+  )
+}
+
+export default App
