345d1a1c9f522c53d1862c9182e568a372569e19
Victor Banerjee <recervictory@gmail.com>
2022-11-23 19:23:20 +0530

    nodemon installed for refresh server on change

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,19 +1,40 @@
-import express from 'express';
+import express from "express";
+
+let articlesInfo = [
+  {
+    name: "learn-react",
+    upvotes: 0,
+  },
+  {
+    name: "learn-node",
+    upvotes: 0,
+  },
+  {
+    name: "mongodb",
+    upvotes: 0,
+  },
+];
 
 const app = express();
 //* IF post have json payload  express convert it to body
 app.use(express.json());
 
-app.get('/hello', (request, response) => {
-    response.send("Hello From Server");
-})
+//* End Point
+app.put('/api/articles/:name/upvote', (request, response) => {
+    console.log("Article Upvote response Triggere...");
+    const {name} = request.params;
+    const article = articlesInfo.find(article => article.name === name);
 
-app.post('/hello', (request, response) => {
-    response.send(`${request.body.name}, You Not Again! `);
-})
+    if (article) {
+        article.upvotes += 1;
+        response.send(`The ${name} article now has upvotes ${article.upvotes}`);
+    } else {
+        response.send(`The ${name} article doesn't exist`);
+    }
+});
 
 app.listen(8000, () => {
-    console.log('====================================');
-    console.log('Server is listening on port 8000');
-    console.log('====================================');
-})
+  console.log("====================================");
+  console.log("Server is listening on port 8000");
+  ("====================================");
+});
