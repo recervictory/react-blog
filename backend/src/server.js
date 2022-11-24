ca4d6b8ab71da6838ef9ac61d01d52129c9f03a1
Victor Banerjee <recervictory@gmail.com>
2022-11-24 11:55:50 +0530

    Mongo DB implemented

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,25 +1,44 @@
 import express from "express";
+import {MongoClient} from "mongodb";
 
 let articlesInfo = [
   {
     name: "learn-react",
     upvotes: 0,
+    comments: [],
   },
   {
     name: "learn-node",
     upvotes: 0,
+    comments: [],
   },
   {
     name: "mongodb",
     upvotes: 0,
+    comments: [],
   },
 ];
+
+
 
 const app = express();
 //* IF post have json payload  express convert it to body
 app.use(express.json());
 
 //* End Point
+app.get('/api/articles/:name', async (request, response) => {
+  const {name} = request.params;
+
+  const client = new MongoClient('mongodb://127.0.0.1:27017');
+  await client.connect();
+
+  const db = client.db('react-blog-db');
+  const article = await db.collection('articles').findOne({name});
+  
+ response.json(article);
+})
+
+
 app.put('/api/articles/:name/upvote', (request, response) => {
     console.log("Article Upvote response Triggere...");
     const {name} = request.params;
@@ -33,8 +52,22 @@ app.put('/api/articles/:name/upvote', (request, response) => {
     }
 });
 
+app.post('/api/articles/:name/comments', (request, response) => {
+    console.log("comment Upvote response Triggere...");
+    const {name} = request.params;
+    const {postedBy, text} = request.body;
+    
+    const article = articlesInfo.find(article => article.name === name);
+    if (article) {
+        article.comments.push({postedBy, text});
+        response.send(article.comments);
+    } else {
+        response.send(`The ${name} article doesn't exist`);
+    }
+})
+
 app.listen(8000, () => {
   console.log("====================================");
   console.log("Server is listening on port 8000");
-  ("====================================");
+  console.log("====================================");
 });
